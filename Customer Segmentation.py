28694dec3a583ec75ea3b435b91807ae99a7d442
vbacho <victorbachour@gmail.com>
2025-03-05 19:43:28 -0800

    working on fixing outliers

--- Customer Segmentation.py
+++ Customer Segmentation.py
@@ -4,18 +4,28 @@ import sklearn.cluster
 import sklearn.preprocessing
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+import os
+os.environ["LOKY_MAX_CPU_COUNT"] = "8"
+
 
 data = pd.read_excel('Online Retail.xlsx')
-data.ffill(inplace=True)
+data = data.ffill()
+
+
+data[['Quantity', 'UnitPrice']] = data[['Quantity', 'UnitPrice']].apply(pd.to_numeric, errors='coerce')
+data.dropna(subset=['Quantity', 'UnitPrice'], inplace=True)
+
+data_filtered = data[(data['Quantity'] > 0) & (data['UnitPrice'] > 0)].copy()
+
 
 scaler = StandardScaler()
-scaled_data = scaler.fit_transform(data[['Quantity', 'UnitPrice']])
+scaled_data = scaler.fit_transform(data_filtered[['Quantity', 'UnitPrice']])
 
-kmeans = KMeans(n_clusters=4,random_state=42)
-data['Cluster'] = kmeans.fit_predict(scaled_data)
+kmeans = KMeans(n_clusters=4,random_state=42, n_init=10)
+data_filtered.loc[:, 'Cluster'] = kmeans.fit_predict(scaled_data)
 
-plt.scatter(data['Quantity'], data['UnitPrice'], c=data['Cluster'], cmap='viridis')
+plt.scatter(data_filtered['Quantity'], data_filtered['UnitPrice'], c=data_filtered['Cluster'], cmap='viridis',alpha=.6)
 plt.xlabel('Quantity')
 plt.ylabel('UnitPrice')
-plt.title('Customer Segmentation')
+plt.title('Customer Segmentation after Outlier Removal')
 plt.show()
