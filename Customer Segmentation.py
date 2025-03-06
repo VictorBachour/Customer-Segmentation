d955b26490de701bae5b82962fee0ec9469ae545
vbacho <victorbachour@gmail.com>
2025-03-05 20:31:46 -0800

    applied log to fix outliers

--- Customer Segmentation.py
+++ Customer Segmentation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn.cluster
@@ -15,7 +16,11 @@ data = data.ffill()
 data[['Quantity', 'UnitPrice']] = data[['Quantity', 'UnitPrice']].apply(pd.to_numeric, errors='coerce')
 data.dropna(subset=['Quantity', 'UnitPrice'], inplace=True)
 
+
+
 data_filtered = data[(data['Quantity'] > 0) & (data['UnitPrice'] > 0)].copy()
+data_filtered['Log_Quantity'] = np.log1p(data_filtered['Quantity'])
+data_filtered['Log_UnitPrice'] = np.log1p(data_filtered['UnitPrice'])
 
 
 scaler = StandardScaler()
@@ -24,8 +29,8 @@ scaled_data = scaler.fit_transform(data_filtered[['Quantity', 'UnitPrice']])
 kmeans = KMeans(n_clusters=4,random_state=42, n_init=10)
 data_filtered.loc[:, 'Cluster'] = kmeans.fit_predict(scaled_data)
 
-plt.scatter(data_filtered['Quantity'], data_filtered['UnitPrice'], c=data_filtered['Cluster'], cmap='viridis',alpha=.6)
-plt.xlabel('Quantity')
-plt.ylabel('UnitPrice')
+plt.scatter(data_filtered['Log_Quantity'], data_filtered['Log_UnitPrice'], c=data_filtered['Cluster'], cmap='viridis',alpha=.6)
+plt.xlabel('Log Quantity')
+plt.ylabel('Log UnitPrice')
 plt.title('Customer Segmentation after Outlier Removal')
 plt.show()
